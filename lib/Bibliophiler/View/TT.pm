a51fa697fd32a83996751509e32fdc039d844fd4
John SJ Anderson <genehack@genehack.org>
2009-11-28 20:28:11 -0500

    default view

---
+++ lib/Bibliophiler/View/TT.pm
@@ -0,0 +1,31 @@
+package Bibliophiler::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+=head1 NAME
+
+Bibliophiler::View::TT - TT View for Bibliophiler
+
+=head1 DESCRIPTION
+
+TT View for Bibliophiler.
+
+=head1 SEE ALSO
+
+L<Bibliophiler>
+
+=head1 AUTHOR
+
+genehack
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
